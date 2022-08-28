0d891bdfc39e2cf44bea5d5b0a961e2f4e0c84f2
Cornel <cornel.cornel200@gmail.com>
2022-08-28 13:49:11 +0300

    clicker v0.5

--- src/main/java/com/main/clickergame/MainController.java
+++ src/main/java/com/main/clickergame/MainController.java
@@ -22,12 +22,43 @@ public class MainController {
     private Text money_count;
 
     @FXML
-    private Button money_upgrade;
+    private Button upgrade_1_clickPower;
+
+    @FXML
+    private Text upgrade_1_cost;
+
+    @FXML
+    private Button upgrade_2_autoClick;
+
+    @FXML
+    private Text upgrade_2_cost;
+
+    @FXML
+    private Text upgrade_3_cost;
+
+    @FXML
+    private Button upgrade_3_image;
     private int money = 0;
+    private int upgrade_1_clickPower_code = 1;
+    private int upgrade_1_cost_code = 20;
     @FXML
     void initialize() {
+            upgrade_1_clickPower.setOnAction(event -> {
+                if(money >= upgrade_1_cost_code) {
+                    money -= upgrade_1_cost_code;
+
+                    money_count.setText(money + "$");
+                    upgrade_1_clickPower_code++;
+                    upgrade_1_clickPower.setText("Click power = " + upgrade_1_clickPower_code);
+
+                    upgrade_1_cost_code = 20 * upgrade_1_clickPower_code;
+                    upgrade_1_cost.setText(upgrade_1_cost_code + "$");
+
+                }
+            });
+
         mob_clicker.setOnMouseClicked(event -> {
-            money++;
+            money+= upgrade_1_clickPower_code;
             money_count.setText(money + "$");
         });
     }
